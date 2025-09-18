99421615c2d7336e100791116c641067c405b63d
09chetan <137068046+09chetan@users.noreply.github.com>
2025-09-18 23:10:05 +0530

    Update ph1.cpp

--- ph1.cpp
+++ ph1.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 class VM
@@ -9,8 +10,8 @@ class VM
 private:
     char buffer[40];
     char Memory[100][4];
-    char IR[4];  // Instruction Register sa
-    char R[4];   // General purpose Register 
+    char IR[4]; // Instruction Register sa
+    char R[4];  // General purpose Register
     bool C;     //  Toggle Register
     int IC;     // Instruction counter
     int SI;     // System Interrupt
@@ -18,7 +19,8 @@ private:
     ofstream outfile;
 
     // Initialize all the variables (buffer, memory, IR, R, IC, C, SI)
-    void init(){
+    void init()
+    {
         fill(buffer, buffer + sizeof(buffer), '\0');
         fill(&Memory[0][0], &Memory[0][0] + sizeof(Memory), '\0');
         fill(IR, IR + sizeof(IR), '\0');
@@ -29,13 +31,14 @@ private:
     }
 
     // Reset buffer to null
-    void resetBuffer(){
+    void resetBuffer()
+    {
         fill(buffer, buffer + sizeof(buffer), '\0');
     }
 
     // Master Mode
     void MOS()
-    { 
+    {
         switch (SI)
         {
         case 1:
@@ -53,75 +56,72 @@ private:
         SI = 0;
     }
 
+    void LOAD()
+    {
+        if (!infile.is_open())
+            return;
 
-    void LOAD(){
-
-        if (infile.is_open())
+        string line;
+        while (getline(infile, line))
         {
-            string s;
-            while (getline(infile, s))
-            { 
-                if (s[0] == '$' && s[1] == 'A' && s[2] == 'M' && s[3] == 'J'){
-                    init();
-                    cout<<"New Job started\n";
-                }
-                else if (s[0] == '$' && s[1] == 'D' && s[2] == 'T' && s[3] == 'A'){
-                    cout << "Data card loding\n";
-                    resetBuffer();
-                    STARTEXE();
-                }
 
-                else if (s[0] == '$' && s[1] == 'E' && s[2] == 'N' && s[3] == 'D'){
-                    cout << "END of Job\n";
-                }
-
-                else{
-
-                    cout << "Program Card loding\n";
-                    int length = s.size();
-                    resetBuffer();  //restting the buffer to null for new instructoins
-
-                    // Buffer <-- Program Card
-                    for (int i = 0; i < length; i++){
-                        buffer[i] = s[i];
-                    }
-
-                    int buff = 0;
-                    int ref = 0;
-                    
-                    // Memory <-- Buffer
-                    while (buff < 40 && buffer[buff] != '\0'){
-                        for (int j = 0; j < 4; j++){
-                            if (buffer[buff] == 'H'){
-                                Memory[IC][j] = 'H';
-                                buff++;
-                                break;
-                            }
-                            Memory[IC][j] = buffer[buff];
-                            buff++;
-                        }
-                        IC++;
-                    }
-                }
-
-                for(int i = 0; i<100; i++)
-                { 
-                    cout<<"M["<<i<<"]\t";
-                    for(int j = 0; j<4; j++ )
-                    {
-                       
-                        cout<<Memory[i][j];
-                        
-                    }
-                    cout<<endl;
-                }
-                
+            // --- START OF JOB ---
+            if (line.substr(0, 4) == "$AMJ")
+            {
+                init();
+                cout << "New Job started\n";
+                outfile.open("output.txt", ios::trunc); // reset output file
+                outfile.close();
             }
-            infile.close(); // Input File closed
+
+            // --- START OF DATA ---
+            else if (line.substr(0, 4) == "$DTA")
+            {
+                cout << "Data card loading\n";
+                resetBuffer();
+                STARTEXE();
+            }
+
+            // --- END OF JOB ---
+            else if (line.substr(0, 4) == "$END")
+            {
+                cout << "END of Job\n";
+            }
+
+            // --- PROGRAM CARD ---
+            else
+            {
+                cout << "Program Card loading\n";
+
+                stringstream ss(line);
+                string instr;
+
+                // Split line into instructions like "GD20", "PD20", "H"
+                while (ss >> instr)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Memory[IC][j] = (j < instr.size()) ? instr[j] : '\0';
+                    }
+                    IC++;
+                }
+
+                // Debug: print just the loaded instructions
+                for (int i = 0; i < IC; i++)
+                {
+                    cout << "M[" << i << "] ";
+                    for (int j = 0; j < 4; j++)
+                        cout << Memory[i][j];
+                    cout << endl;
+                }
+            }
         }
+
+        infile.close(); // close input file
     }
 
-    void STARTEXE(){
+    void STARTEXE()
+    {
         IC = 0;
         EXECUTEUSERPROGRAM();
     }
@@ -177,69 +177,86 @@ private:
         outfile.close();
     }
 
-    void EXECUTEUSERPROGRAM(){  // Slave Mode
-        while (IC < 99 && Memory[IC][0] != '\0'){
+    void EXECUTEUSERPROGRAM()
+    { // Slave Mode
+        while (IC < 99 && Memory[IC][0] != '\0')
+        {
 
-            for (int i = 0; i < 4; i++){
+            for (int i = 0; i < 4; i++)
+            {
                 IR[i] = Memory[IC][i];
             }
 
             IC++;
 
-            //SI= 1-GD, 2-PD, 3-H
+            // SI= 1-GD, 2-PD, 3-H
 
             // GD
-            if (IR[0] == 'G' && IR[1] == 'D'){
+            if (IR[0] == 'G' && IR[1] == 'D')
+            {
                 SI = 1;
                 MOS();
             }
 
             // PD
-            else if (IR[0] == 'P' && IR[1] == 'D'){
+            else if (IR[0] == 'P' && IR[1] == 'D')
+            {
                 SI = 2;
                 MOS();
             }
 
-            // H 
-            else if (IR[0] == 'H'){
+            // H
+            else if (IR[0] == 'H')
+            {
                 SI = 3;
                 MOS();
                 return;
             }
 
             // LR - LOAD DATA (R <-- memory[IR[2,3]])
-            else if (IR[0] == 'L' && IR[1] == 'R'){
-                for (int i = 0; i < 4; i++){
+            else if (IR[0] == 'L' && IR[1] == 'R')
+            {
+                for (int i = 0; i < 4; i++)
+                {
                     R[i] = Memory[(IR[2] - '0') * 10 + (IR[3] - '0')][i];
                 }
             }
 
             // SR - STORE (memory[IR[2,3]] <-- R)
-            else if (IR[0] == 'S' && IR[1] == 'R'){
-                for (int i = 0; i < 4; i++){
+            else if (IR[0] == 'S' && IR[1] == 'R')
+            {
+                for (int i = 0; i < 4; i++)
+                {
                     Memory[(IR[2] - '0') * 10 + (IR[3] - '0')][i] = R[i];
                 }
             }
 
             // CR - COMPARE(R, memory[IR[2,3]])
-            else if (IR[0] == 'C' && IR[1] == 'R'){
+            else if (IR[0] == 'C' && IR[1] == 'R')
+            {
                 int cnt = 0;
-                for (int i = 0; i < 4; i++){
-                    if (Memory[(IR[2] - '0') * 10 + (IR[3] - '0')][i] == R[i]){
+                for (int i = 0; i < 4; i++)
+                {
+                    if (Memory[(IR[2] - '0') * 10 + (IR[3] - '0')][i] == R[i])
+                    {
                         cnt++;
                     }
                 }
-                if (cnt == 4){
+                if (cnt == 4)
+                {
                     C = true;
                 }
-                else{
+                else
+                {
                     C = false;
                 }
             }
 
             // BT (JUMP if toogle is T)
-            else if (IR[0] == 'B' && IR[1] == 'T'){
-                if (C){
+            else if (IR[0] == 'B' && IR[1] == 'T')
+            {
+                if (C)
+                {
                     IC = (IR[2] - '0') * 10 + (IR[3] - '0');
                 }
             }
